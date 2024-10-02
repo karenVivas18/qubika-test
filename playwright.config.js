155ca1c3887bd4b0ac4616e8a415e8b033c1969e
KarenVivas <119443398+karenVivas18@users.noreply.github.com>
2024-10-02 14:14:29 -0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ module.exports = defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    headless: false, 
+    headless: true, 
     trace: 'on-first-retry',
   },
 
